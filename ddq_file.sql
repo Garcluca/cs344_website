792e766bf17d42becad074d81fa68a7189bf29d8
christinelant <christinelantigua@yahoo.com>
2021-05-13 16:50:30 -0400

    updated insert for tables payments and orders

--- ddq_file.sql
+++ ddq_file.sql
@@ -6,19 +6,34 @@ DROP TABLE IF EXISTS orderItems;
 DROP TABLE IF EXISTS payments;
 DROP TABLE IF EXISTS orders;
 
---Create Tables
+-- Table Creation
 
 CREATE TABLE orders (
-    id INT(11) NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     dateOrdered DATE NOT NULL,
-    CONSTRAINT `customerID` FOREIGN KEY (customerID) REFERENCES customer (id)
+    customerID INT,
+
+    FOREIGN KEY(customerID) REFERENCES customer(id)
 )
 
 CREATE TABLE payments (
-    id INT(11) NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    customerID INT(11),
     amountPaid DOUBLE NOT NULL,
     datePaid DATE NOT NULL,
-    paymentType VARCHAR(255) NOT NULL
+    paymentType TEXT NOT NULL,
     
-    CONSTRAINT `orderID` FOREIGN KEY (orderID) REFERENCES order (id)
+    FOREIGN KEY(customerID) REFERENCES customer(id)
 )
+
+--- Insert data into Tables
+
+INSERT INTO orders (dateOrdered)
+VALUES ('2021-02-12'),
+        ('2020-04-12'),
+        ('2021-02-12');
+
+INSERT INTO payments (amountPaid, datePaid, paymentType)
+VALUES ('3.21','2021-04-21','CASH'),
+        ('9.29','2021-04-21','CREDIT'),
+        ('15.29','2021-03-21','CASH');
